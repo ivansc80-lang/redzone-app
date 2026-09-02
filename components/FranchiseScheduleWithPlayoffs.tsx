b058c6533135e9ae79119320ad49a21e19e80952
ivansc80-lang <ivansc80@gmail.com>
2026-09-02 15:37:21 +0200

    FRANQUICIAS: preparar integración aislada de calendario playoff

---
+++ components/FranchiseScheduleWithPlayoffs.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+// Integración de calendario playoff preparada de forma aislada.
+// FranchiseScheduleBase conserva exactamente el HOME existente.
+// Este archivo será el punto de conexión cuando validemos el bloque visual,
+// sin tocar PRÓXIMO PARTIDO ni sus estadísticas.
+
+export { default } from "./FranchiseScheduleBase";
